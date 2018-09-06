157bf9ec589e200576c1cb3068f920c3705417f0
Jayden Szekely <jayden@deslab.co.nz>
2018-09-06 16:27:40 +1200

    Working
    Working on it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import GroupBar from './components/GroupBar';
+import Login from './components/Login';
+import Home from './components/Home';
 import AssetManager from './components/AssetManager';
 import ComposeBar from './components/ComposeBar';
 import MessageStream from './components/MessageStream';
 import './App.css';
 
+
+const Stream = () => (
+  <div>
+    <GroupBar />
+    <AssetManager />
+    <ComposeBar />
+    <MessageStream />
+  </div>
+)
+
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <NavBar />
-        <GroupBar />
-        <AssetManager />
-        <ComposeBar />
-        <MessageStream />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <NavBar />
+          <Route path='/stream' component={Stream} />
+          <Route path='/login' component={Login} />
+          <Route path='/' component={Home} />
+        </div>
+      </BrowserRouter>
+      
     );
   }
 }
-
 
 
 export default App;
